9d45c54ccf0d615106790338dcbccbd3d95839f6
MdTabish24 <fiza8983khan@gmail.com>
2025-12-14 00:47:52 +0530

    Fix: Add API root endpoint

--- insightstream/urls.py
+++ insightstream/urls.py
@@ -4,9 +4,26 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from django.views.static import serve
+from django.http import JsonResponse
+
+def api_root(request):
+    return JsonResponse({
+        'message': 'InsightStream API',
+        'version': '1.0',
+        'endpoints': {
+            'users': '/api/users/',
+            'thumbnails': '/api/thumbnails/',
+            'content': '/api/content/',
+            'keywords': '/api/keywords/',
+            'hashtags': '/api/hashtags/',
+            'analytics': '/api/analytics/',
+            'admin': '/api/admin-dashboard/',
+        }
+    })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', api_root),
     path('api/users/', include('apps.users.urls')),
     path('api/thumbnails/', include('apps.thumbnails.urls')),
     path('api/content/', include('apps.content.urls')),
